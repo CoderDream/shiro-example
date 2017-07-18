37f84261d70d33310c694886a0d5a035e8cf491a
CoderDream <coderdream@gmail.com>
2017-07-18 17:48:40 +0800

    update

--- shiro-example-chapter3/src/main/java/com/github/zhangkaitao/shiro/chapter3/permission/MyRolePermissionResolver.java
+++ shiro-example-chapter3/src/main/java/com/github/zhangkaitao/shiro/chapter3/permission/MyRolePermissionResolver.java
@@ -8,16 +8,19 @@ import java.util.Arrays;
 import java.util.Collection;
 
 /**
- * <p>User: Zhang Kaitao
- * <p>Date: 14-1-26
- * <p>Version: 1.0
+ * <p>
+ * User: Zhang Kaitao
+ * <p>
+ * Date: 14-1-26
+ * <p>
+ * Version: 1.0
  */
 public class MyRolePermissionResolver implements RolePermissionResolver {
-    @Override
-    public Collection<Permission> resolvePermissionsInRole(String roleString) {
-        if("role1".equals(roleString)) {
-            return Arrays.asList((Permission)new WildcardPermission("menu:*"));
-        }
-        return null;
-    }
+	@Override
+	public Collection<Permission> resolvePermissionsInRole(String roleString) {
+		if ("role1".equals(roleString)) {
+			return Arrays.asList((Permission) new WildcardPermission("menu:*"));
+		}
+		return null;
+	}
 }
