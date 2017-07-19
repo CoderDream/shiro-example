1859b97d5a01bd285a6cb02585ac68a25932a398
CoderDream <coderdream@gmail.com>
2017-07-19 15:38:06 +0800

    update chapter17

--- shiro-example-chapter16/src/main/java/com/github/zhangkaitao/shiro/chapter16/web/bind/annotation/CurrentUser.java
+++ shiro-example-chapter16/src/main/java/com/github/zhangkaitao/shiro/chapter16/web/bind/annotation/CurrentUser.java
@@ -21,21 +21,25 @@ import com.github.zhangkaitao.shiro.chapter16.Constants;
 import java.lang.annotation.*;
 
 /**
- * <p>绑定当前登录的用户</p>
- * <p>不同于@ModelAttribute</p>
+ * <p>
+ * 绑定当前登录的用户
+ * </p>
+ * <p>
+ * 不同于@ModelAttribute
+ * </p>
  *
  * @author Zhang Kaitao
  */
-@Target({ElementType.PARAMETER})
+@Target({ ElementType.PARAMETER })
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface CurrentUser {
 
-    /**
-     * 当前用户在request中的名字
-     *
-     * @return
-     */
-    String value() default Constants.CURRENT_USER;
+	/**
+	 * 当前用户在request中的名字
+	 *
+	 * @return
+	 */
+	String value() default Constants.CURRENT_USER;
 
 }
--- shiro-example-chapter16/src/main/java/com/github/zhangkaitao/shiro/chapter16/web/bind/method/CurrentUserMethodArgumentResolver.java
+++ shiro-example-chapter16/src/main/java/com/github/zhangkaitao/shiro/chapter16/web/bind/method/CurrentUserMethodArgumentResolver.java
@@ -8,27 +8,36 @@ import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
 /**
- * <p>用于绑定@FormModel的方法参数解析器
- * <p>User: Zhang Kaitao
- * <p>Date: 13-1-12 下午5:01
- * <p>Version: 1.0
+ * <p>
+ * 用于绑定@FormModel的方法参数解析器
+ * <p>
+ * User: Zhang Kaitao
+ * <p>
+ * Date: 13-1-12 下午5:01
+ * <p>
+ * Version: 1.0
  */
-public class CurrentUserMethodArgumentResolver implements HandlerMethodArgumentResolver {
+public class CurrentUserMethodArgumentResolver
+		implements HandlerMethodArgumentResolver {
 
-    public CurrentUserMethodArgumentResolver() {
-    }
+	public CurrentUserMethodArgumentResolver() {
+	}
 
-    @Override
-    public boolean supportsParameter(MethodParameter parameter) {
-        if (parameter.hasParameterAnnotation(CurrentUser.class)) {
-            return true;
-        }
-        return false;
-    }
+	@Override
+	public boolean supportsParameter(MethodParameter parameter) {
+		if (parameter.hasParameterAnnotation(CurrentUser.class)) {
+			return true;
+		}
+		return false;
+	}
 
-    @Override
-    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-        CurrentUser currentUserAnnotation = parameter.getParameterAnnotation(CurrentUser.class);
-        return webRequest.getAttribute(currentUserAnnotation.value(), NativeWebRequest.SCOPE_REQUEST);
-    }
+	@Override
+	public Object resolveArgument(MethodParameter parameter,
+			ModelAndViewContainer mavContainer, NativeWebRequest webRequest,
+			WebDataBinderFactory binderFactory) throws Exception {
+		CurrentUser currentUserAnnotation = parameter
+				.getParameterAnnotation(CurrentUser.class);
+		return webRequest.getAttribute(currentUserAnnotation.value(),
+				NativeWebRequest.SCOPE_REQUEST);
+	}
 }
