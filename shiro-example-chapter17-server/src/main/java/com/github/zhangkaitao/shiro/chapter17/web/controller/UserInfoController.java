901b059473c81a02ef08ddfc3456ec6727b7d834
CoderDream <coderdream@gmail.com>
2017-07-19 16:07:33 +0800

    update chapter17

--- shiro-example-chapter17-server/src/main/java/com/github/zhangkaitao/shiro/chapter17/web/controller/UserInfoController.java
+++ shiro-example-chapter17-server/src/main/java/com/github/zhangkaitao/shiro/chapter17/web/controller/UserInfoController.java
@@ -23,66 +23,74 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * <p>User: Zhang Kaitao
- * <p>Date: 14-2-16
- * <p>Version: 1.0
+ * <p>
+ * User: Zhang Kaitao
+ * <p>
+ * Date: 14-2-16
+ * <p>
+ * Version: 1.0
  */
 @RestController
 public class UserInfoController {
 
-    @Autowired
-    private OAuthService oAuthService;
+	@Autowired
+	private OAuthService oAuthService;
 
-    @RequestMapping("/userInfo")
-    public HttpEntity userInfo(HttpServletRequest request) throws OAuthSystemException {
-        try {
+	@RequestMapping("/userInfo")
+	public HttpEntity userInfo(HttpServletRequest request)
+			throws OAuthSystemException {
+		try {
 
-            //构建OAuth资源请求
-            OAuthAccessResourceRequest oauthRequest = new OAuthAccessResourceRequest(request, ParameterStyle.QUERY);
-            //获取Access Token
-            String accessToken = oauthRequest.getAccessToken();
+			// 构建OAuth资源请求
+			OAuthAccessResourceRequest oauthRequest = new OAuthAccessResourceRequest(
+					request, ParameterStyle.QUERY);
+			// 获取Access Token
+			String accessToken = oauthRequest.getAccessToken();
 
-            //验证Access Token
-            if (!oAuthService.checkAccessToken(accessToken)) {
-                // 如果不存在/过期了，返回未验证错误，需重新验证
-                OAuthResponse oauthResponse = OAuthRSResponse
-                        .errorResponse(HttpServletResponse.SC_UNAUTHORIZED)
-                        .setRealm(Constants.RESOURCE_SERVER_NAME)
-                        .setError(OAuthError.ResourceResponse.INVALID_TOKEN)
-                        .buildHeaderMessage();
+			// 验证Access Token
+			if (!oAuthService.checkAccessToken(accessToken)) {
+				// 如果不存在/过期了，返回未验证错误，需重新验证
+				OAuthResponse oauthResponse = OAuthRSResponse
+						.errorResponse(HttpServletResponse.SC_UNAUTHORIZED)
+						.setRealm(Constants.RESOURCE_SERVER_NAME)
+						.setError(OAuthError.ResourceResponse.INVALID_TOKEN)
+						.buildHeaderMessage();
 
-                HttpHeaders headers = new HttpHeaders();
-                headers.add(OAuth.HeaderType.WWW_AUTHENTICATE, oauthResponse.getHeader(OAuth.HeaderType.WWW_AUTHENTICATE));
-                return new ResponseEntity(headers, HttpStatus.UNAUTHORIZED);
-            }
-            //返回用户名
-            String username = oAuthService.getUsernameByAccessToken(accessToken);
-            return new ResponseEntity(username, HttpStatus.OK);
-        } catch (OAuthProblemException e) {
-            //检查是否设置了错误码
-            String errorCode = e.getError();
-            if (OAuthUtils.isEmpty(errorCode)) {
-                OAuthResponse oauthResponse = OAuthRSResponse
-                        .errorResponse(HttpServletResponse.SC_UNAUTHORIZED)
-                        .setRealm(Constants.RESOURCE_SERVER_NAME)
-                        .buildHeaderMessage();
+				HttpHeaders headers = new HttpHeaders();
+				headers.add(OAuth.HeaderType.WWW_AUTHENTICATE, oauthResponse
+						.getHeader(OAuth.HeaderType.WWW_AUTHENTICATE));
+				return new ResponseEntity(headers, HttpStatus.UNAUTHORIZED);
+			}
+			// 返回用户名
+			String username = oAuthService
+					.getUsernameByAccessToken(accessToken);
+			return new ResponseEntity(username, HttpStatus.OK);
+		} catch (OAuthProblemException e) {
+			// 检查是否设置了错误码
+			String errorCode = e.getError();
+			if (OAuthUtils.isEmpty(errorCode)) {
+				OAuthResponse oauthResponse = OAuthRSResponse
+						.errorResponse(HttpServletResponse.SC_UNAUTHORIZED)
+						.setRealm(Constants.RESOURCE_SERVER_NAME)
+						.buildHeaderMessage();
 
-                HttpHeaders headers = new HttpHeaders();
-                headers.add(OAuth.HeaderType.WWW_AUTHENTICATE, oauthResponse.getHeader(OAuth.HeaderType.WWW_AUTHENTICATE));
-                return new ResponseEntity(headers, HttpStatus.UNAUTHORIZED);
-            }
+				HttpHeaders headers = new HttpHeaders();
+				headers.add(OAuth.HeaderType.WWW_AUTHENTICATE, oauthResponse
+						.getHeader(OAuth.HeaderType.WWW_AUTHENTICATE));
+				return new ResponseEntity(headers, HttpStatus.UNAUTHORIZED);
+			}
 
-            OAuthResponse oauthResponse = OAuthRSResponse
-                    .errorResponse(HttpServletResponse.SC_UNAUTHORIZED)
-                    .setRealm(Constants.RESOURCE_SERVER_NAME)
-                    .setError(e.getError())
-                    .setErrorDescription(e.getDescription())
-                    .setErrorUri(e.getUri())
-                    .buildHeaderMessage();
+			OAuthResponse oauthResponse = OAuthRSResponse
+					.errorResponse(HttpServletResponse.SC_UNAUTHORIZED)
+					.setRealm(Constants.RESOURCE_SERVER_NAME)
+					.setError(e.getError())
+					.setErrorDescription(e.getDescription())
+					.setErrorUri(e.getUri()).buildHeaderMessage();
 
-            HttpHeaders headers = new HttpHeaders();
-            headers.add(OAuth.HeaderType.WWW_AUTHENTICATE, oauthResponse.getHeader(OAuth.HeaderType.WWW_AUTHENTICATE));
-            return new ResponseEntity(HttpStatus.BAD_REQUEST);
-        }
-    }
+			HttpHeaders headers = new HttpHeaders();
+			headers.add(OAuth.HeaderType.WWW_AUTHENTICATE,
+					oauthResponse.getHeader(OAuth.HeaderType.WWW_AUTHENTICATE));
+			return new ResponseEntity(HttpStatus.BAD_REQUEST);
+		}
+	}
 }
